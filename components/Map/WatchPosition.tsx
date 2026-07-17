421367c534b95838a92f45ac937c84711ee6d68c
jstiava <j.j.stiava@wustl.edu>
2026-07-17 10:25:02 -0500

    syntax error to allow build

--- components/Map/WatchPosition.tsx
+++ components/Map/WatchPosition.tsx
@@ -59,7 +59,7 @@ export default function WatchUserPosition({
                                     properties: {},
                                 },
                             ],
-                        };
+                        } as any;
 
                         (
                             map.getSource(sourceId) as maplibregl.GeoJSONSource
